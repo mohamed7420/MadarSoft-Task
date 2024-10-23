c1743c659ecd31dd664d21921ed707fe37ee24ea
mohamed osama <mabdulmajid.c@nwc.com.sa>
2024-10-23 19:33:24 +0300

    Initial Commit

---
+++ MadarSoftTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MadarSoftTask
+//
+//  Created by Mohamed Osama on 23/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
