1935f68c4dbabe7b304af131ed3c8557ce171d4b
mohamed osama <mabdulmajid.c@nwc.com.sa>
2024-10-24 12:13:20 +0300

    creating colors extension file

---
+++ MadarSoftTask/Helper/Colors+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Colors+Extension.swift
+//  MadarSoftTask
+//
+//  Created by Mohamed Osama on 23/10/2024.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    public static let appBackground: UIColor = .systemBackground
+}
