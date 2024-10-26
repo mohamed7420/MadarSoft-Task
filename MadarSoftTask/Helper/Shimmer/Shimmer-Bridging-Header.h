170d328a558dea7f6d3a0139901e9313f8d0d5de
mohamed osama <mabdulmajid.c@nwc.com.sa>
2024-10-26 16:01:54 +0300

    adding shimmer objective c files and bridging header file

---
+++ MadarSoftTask/Helper/Shimmer/Shimmer-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  shimmirBridgingFile.h
+//  MadarSoftTask
+//
+//  Created by Mohamed Osama on 24/10/2024.
+//
+
+
+#import "FBShimmering.h"
+#import "FBShimmeringLayer.h"
+#import "FBShimmeringView.h"
+
